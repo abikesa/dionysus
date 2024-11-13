93d22c93d04212bcbdcbb88cfe978b2ac4282e57
abikesa <abikesa.sh@gmail.com>
2024-11-13 16:57:22 -0500

    testbin, take 6 . bug identified

--- kitabo/ensi/scripts/jb_clean.sh
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -3,7 +3,7 @@ cp -r figures/* _build/html/figures/
 cp -r pdfs/* _build/html/pdfs/
 cp -r media/* _build/html/media/
 # cp -r act1/app/* _build/html/act1/app
-cp -r data/* _build/html/data
+# cp -r data/* _build/html/data
 cp -r testbin/* _build/html/testbin
 
 
